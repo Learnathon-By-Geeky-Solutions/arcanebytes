a53e50ef2cd08108fb82e263e027b2a39114b0f8
tanzilrayhan <tanzilrayhan169@gmail.com>
2025-02-17 02:01:51 +0600

    Leaderboard preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Hero from '../components/hero'
 import FeaturesOverview from '../components/featuresOverview'
+import LeadernoardPreview from '../components/leaderboardPreview'
 const page = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
       <FeaturesOverview/>
+      <LeadernoardPreview/>
     </div>
   )
 }
---
+++ src/components/leaderboardPreview.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Kelly_Slab } from "next/font/google";
+import Image from 'next/image';
+const kelly = Kelly_Slab({
+    weight: ["400"],
+  });
+
+const leaderboardPreview = () => {
+  return (
+    <div className='my-20 flex flex-col items-center'>
+        <h1 className={`${kelly.className} rounded-lg max-w-2xl text-[#202020] text-center accent-theme py-5 px-20 text-5xl`}>
+            LeaderBoard Preview 
+        </h1>
+        <section className='flex flex-col items-center my-20'>
+            <Image className='mb-10' src="/Top 3.png" width={500} height={200} />
+            <Image src="/Leaderboard.png" width={1000} height={500} />
+        </section>
+    </div>
+  )
+}
+
+export default leaderboardPreview
