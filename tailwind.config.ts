67ae7bdef0e64afa4c56a9e316c4d7069c8e941b
tanzilrayhan <tanzilrayhan169@gmail.com>
2025-02-16 23:29:48 +0600

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+import daisyui from "daisyui"
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  daisyui: {
+    themes: [],
+  },
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [
+    daisyui
+  ],
+} satisfies Config;
